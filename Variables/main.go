82d7288b8b56e81900a0319d87ca49c1de8a9495
Suhagshetty <suhagshetty07@gmail.com>
2026-05-27 12:07:14 +0530

    variables in go and int float and string

---
+++ Variables/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Print("Welcome to the variables section\n")
+
+	// Variables:- var
+	// \n helps us to get to next line or u can use Println instead of Print.
+
+	// String
+
+	var name string = "Suhag  S Shetty"
+	fmt.Print("The name is: ", name, "\n")
+
+	// Int
+	var year int = 2026
+	fmt.Print("The current year is: ", year, "\n")
+
+	// Float
+	var rating float64 = 4.9
+	fmt.Print("The rating is ", rating, "\n")
+}
